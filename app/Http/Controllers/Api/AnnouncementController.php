dcdf09a679b5ac22400b66d7041cbf0c79900130
Adam Rasyid <adamrasyid01@gmail.com>
2025-09-09 12:07:43 +0700

    feat : Announcement Controller

---
+++ app/Http/Controllers/Api/AnnouncementController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\AnnouncementResource;
+use App\Models\Announcement;
+use Illuminate\Http\Request;
+
+class AnnouncementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        try{
+            $announcements = Announcement::all();
+            return ResponseFormatter::success(AnnouncementResource::collection($announcements), 'Announcements retrieved successfully.');
+
+        }catch(\Exception $e){
+            return ResponseFormatter::error($e->getMessage(), 'Failed to retrieve announcements.', 500);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
